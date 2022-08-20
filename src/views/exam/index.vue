38e4e9b3ad5da7de949142512576c2f80da1b268
milk <gongzhenyu40@qq.com>
2022-08-20 14:34:52 +0800

    wip: 2

---
+++ src/views/exam/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+	import { startExam } from "@/api";
+
+	export default {
+		name: "exam",
+		data() {
+			return {
+				examMin: '00:00',
+        questionList: []
+			};
+		},
+    created () {
+      startExam({
+				userId: localStorage.getItem('userId')
+			}).then((res) => {
+				console.log(res);
+			});
+    },
+	};
+</script>
+
+<style lang="scss" scoped>
+</style>
