65f0a8230ece87df743e4bcd4ff509c525db1cf8
zhongwaner <vanny_w@163.com>
2022-08-20 13:58:49 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,36 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [{}]
-
 const router = new VueRouter({
-  routes
+  routes: [{
+    path: '/login', // 登录页
+    name: 'login',
+    component: resolve => require(['@/views/login'], resolve)
+  }, {
+    path: '/',
+    // component: resolve => require(['@/views/Home'], resolve),
+    children: [{}]
+  }]
 })
+
+// 监测没有token默认跳转登录页
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token') || ''
+  if(!token) {
+    if(to.name === 'login') {
+      next()
+    }else {
+      next({name: 'login'})
+    }
+  }else {
+    if(to.name === 'login') {
+      next({path: '/'})
+    }else {
+      next()
+    }
+  }
+})
+
+
 
 export default router
