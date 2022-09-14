2486b6eaab559b673b44116af888b39716362c1d
milk <gongzhenyu40@qq.com>
2022-09-14 17:35:40 +0800

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: '考试系统'
+    }
+  },
   lintOnSave: false,
   devServer: {
     proxy: {
